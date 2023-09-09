4025e8f7c1a83fcd91bab2aaf02b3a710478f601
Harsh Telang <telangharsh06@gmail.com>
2023-09-09 22:20:47 +0530

    files edited

---
+++ src/app/login/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import {useRouter} from "next/navigation";
+import {axios} from "axios";
+
+
+
+
+export default function LoginPage() {
+    const [user, setUser] = React.useState({
+        email: "",
+        password:"",
+       
+    })
+    const onLogin =async () => {
+        
+    }
+
+    return(
+        <div className="flex justify-center items-center flex-col min-h-screen py-2 bg-slate-700">
+           <h1>Login</h1>
+           <hr />
+           
+            <label htmlFor="email">Email</label>
+           <input className="p-2 border border-gray-300 rounded-lg mb-4"
+           id="email" type="text" value={user.email} onChange={(e) => setUser({...user, email: e.target.value})} placeholder="email"
+            />
+            <label htmlFor="username">Password</label>
+           <input className="p-2 border border-gray-300 rounded-lg mb-4"
+           id="password" type="text" value={user.password} onChange={(e) => setUser({...user, password: e.target.value})} placeholder="password"
+            />
+            <button 
+           onClick={onLogin}
+            className="p-2 border border-gray-300 rounded-lg mb-4">Login here</button>
+            <Link href="/signup">Visit Signup Page</Link>
+        </div>
+    )
+}
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,12 +4,18 @@ import React from "react";
 import {useRouter} from "next/navigation";
 import {axios} from "axios";
 
-export default function LoginPage() {
+
+
+
+export default function SignupPage() {
     const [user, setUser] = React.useState({
         email: "",
         password:"",
         username:"",
     })
+    const onSignup =async () => {
+        
+    }
 
     return(
         <div className="flex justify-center items-center flex-col min-h-screen py-2">
@@ -23,12 +29,12 @@ export default function LoginPage() {
            <input className="p-2 border border-gray-300 rounded-lg mb-4"
            id="email" type="text" value={user.email} onChange={(e) => setUser({...user, email: e.target.value})} placeholder="email"
             />
-            <label htmlFor="username">Username</label>
+            <label htmlFor="username">Password</label>
            <input className="p-2 border border-gray-300 rounded-lg mb-4"
            id="password" type="text" value={user.password} onChange={(e) => setUser({...user, password: e.target.value})} placeholder="password"
             />
             <button 
-            onClick={onSignup}
+           onClick={onSignup}
             className="p-2 border border-gray-300 rounded-lg mb-4">Signup here</button>
             <Link href="/login">Visit Login Page</Link>
         </div>
