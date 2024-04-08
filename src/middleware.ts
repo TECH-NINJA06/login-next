3c0d607ded57cdcea7e68318f166cc8a4302ca8e
Harsh Telang <125661668+TECH-NINJA06@users.noreply.github.com>
2024-04-08 23:13:03 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,5 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
  
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname
 
@@ -17,14 +15,13 @@ export function middleware(request: NextRequest) {
 
     // no token
     if (!isPublicPath && !token){
-        return NextResponse.redirect(new URL('/', request.nextUrl))
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
     }
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: [
-    '/',
     '/profile',
     '/login',
     '/signup',
